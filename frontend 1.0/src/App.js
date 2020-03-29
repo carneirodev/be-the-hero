4c04e952b74783cc3798566ab7c884949bdbc592
viniciusPC <viniciuscarneirodasilva@gmail.com>
2020-03-29 17:40:20 -0300

    Finalizado

---
+++ frontend 1.0/src/App.js
@@ -0,0 +1,15 @@
+import React, {useState}from 'react';
+
+// JSX (Javascript XML) -Quando o HTML esta intregado dentro do javascript
+import './global.css'
+import Routes from './routes';
+
+function App() {
+  
+  return (
+   <Routes />
+    
+  );
+}
+
+export default App;
